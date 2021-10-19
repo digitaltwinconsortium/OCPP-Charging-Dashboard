a0de520df47dcf74780ddb0309c60d61a8d3bafe
Erich Barnstedt <erichb@microsoft.com>
2021-10-19 08:08:35 +0200

    Initial commit.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace OpcUaWebDashboard.Controllers
+{
+    public class StatusHub : Hub
+    {
+    }
+
+    public class DashboardController : Controller
+    {
+        private static IHubContext<StatusHub> _hubContext;
+        private static List<Tuple<string, string, string>> _latestTelemetry;
+        private static Stopwatch _tableTimer;
+        private static bool _pageReloaded = false;
+
+        public DashboardController(IHubContext<StatusHub> hubContext)
+        {
+            _hubContext = hubContext;
+            _latestTelemetry = new List<Tuple<string, string, string>>();
+            _tableTimer = new Stopwatch();
+            _tableTimer.Start();
+        }
+
+        public static void AddDatasetToChart(string name)
+        {
+            _hubContext.Clients.All.SendAsync("addDatasetToChart", name).GetAwaiter().GetResult();
+            Debug.WriteLine("Sent new data set: " + name);
+        }
+
+        public static void AddDataToChart(string timestamp, float[] values)
+        {
+            _hubContext.Clients.All.SendAsync("addDataToChart", timestamp, values).GetAwaiter().GetResult();
+        }
+
+        public ActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+
+        public ActionResult Index()
+        {
+            _pageReloaded = true;
+
+            return View("Index");
+        }
+
+        public static void CreateTableForTelemetry(List<Tuple<string,string, string>> telemetry)
+        {
+            foreach (Tuple<string, string, string> item in telemetry)
+            {
+                bool found = false;
+                for (int i = 0; i < _latestTelemetry.Count; i++)
+                {
+                    if (_latestTelemetry[i].Item1 == item.Item1)
+                    {
+                        _latestTelemetry[i] = item;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    _latestTelemetry.Add(item);
+                }
+            }
+
+            // update table every second
+            if (_tableTimer.ElapsedMilliseconds > 1000)
+            {
+                // create HTML table
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<table width='800px' cellpadding='3' cellspacing='3'>");
+
+                // header
+                sb.Append("<tr>");
+                sb.Append("<th><b>OPC UA Node ID</b></th>");
+                sb.Append("<th><b>Latest Value</b></th>");
+                sb.Append("<th><b>Time Stamp</b></th>");
+                sb.Append("</tr>");
+
+                // rows
+                foreach (Tuple<string, string, string> item in _latestTelemetry)
+                {
+                    sb.Append("<tr>");
+                    sb.Append("<td style='width:400px'>" + item.Item1 + "</td>");
+                    sb.Append("<td style='width:200px'>" + item.Item2 + "</td>");
+                    sb.Append("<td style='width:200px'>" + item.Item3 + "</td>");
+                    sb.Append("</tr>");
+                }
+
+                sb.Append("</table>");
+
+                _hubContext.Clients.All.SendAsync("addTable", sb.ToString()).GetAwaiter().GetResult();
+
+                // if the page was reloaded in the mean-time, resend the data sets
+                if (_pageReloaded == true)
+                {
+                    foreach (string nodeId in MessageProcessor.NodeIDs)
+                    {
+                        AddDatasetToChart(nodeId);
+                    }
+
+                    _pageReloaded = false;
+                }
+
+                _tableTimer.Restart();
+            }
+        }
+    }
+}
---
+++ MessageProcessor.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using Newtonsoft.Json;
+using OCPPCentralSystem.Schemas.DTDL;
+using OpcUaWebDashboard.Controllers;
+using OpcUaWebDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpcUaWebDashboard
+{
+    /// <summary>
+    /// This class processes all ingested data into IoTHub.
+    /// </summary>
+    public class MessageProcessor : IEventProcessor
+    {
+        private Stopwatch _checkpointStopwatch = new Stopwatch();
+        private const double _checkpointPeriodInMinutes = 5;
+
+        public static List<string> NodeIDs = new List<string>();
+        private static List<float> _currentValues = new List<float>();
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            // get number of messages between checkpoints
+            _checkpointStopwatch.Start();
+
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Trace.TraceError($"Message processor error '{error.Message}' on partition with id '{context.PartitionId}'");
+            Trace.TraceError($"Exception stack '{error.StackTrace}'");
+            return Task.CompletedTask;
+        }
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        private void ProcessPublisherMessage(OCPPChargePoint publisherMessage)
+        {
+            List<SignalRModel> receivedDataItems = new List<SignalRModel>();
+            List<Tuple<string, string, string>> tableEntries = new List<Tuple<string, string, string>>();
+
+            string samsonURI = "http://samsongroup.com/opc_ua_SAM_FLEXPOS_DIM/";
+            Dictionary<string, string> displayNameMap = new Dictionary<string, string>();
+            displayNameMap.Add(samsonURI + "#i=6076", "SAMSON Trovis ActDynStressFactor (i=6076)");
+            displayNameMap.Add(samsonURI + "#i=6112", "SAMSON Trovis DiagnosticStatus (i=6112)");
+            displayNameMap.Add(samsonURI + "#i=6092", "SAMSON Trovis ActValvePosition (i=6092)");
+            displayNameMap.Add(samsonURI + "#i=6066", "SAMSON Trovis ActPressureOut2 (i=6066)");
+            displayNameMap.Add(samsonURI + "#i=6061", "SAMSON Trovis ActPressureOut1 (i=6061)");
+            displayNameMap.Add(samsonURI + "#i=6071", "SAMSON Trovis ActSupplyPressure (i=6071)");
+            displayNameMap.Add(samsonURI + "#i=6102", "SAMSON Trovis SetValvePosition (i=6102)");
+            displayNameMap.Add(samsonURI + "#i=6097", "SAMSON Trovis ActControlDeviation (i=6097)");
+
+            // unbatch the received data
+            foreach (Message message in publisherMessage.Messages)
+            {
+                foreach (string nodeId in message.Payload.Keys)
+                {
+                    // make sure we have it in our list of nodeIDs, which form the basis of our individual time series datasets
+                    if (!NodeIDs.Contains(displayNameMap[nodeId]))
+                    {
+                        NodeIDs.Add(displayNameMap[nodeId]);
+                        _currentValues.Add(0.0f);
+                        DashboardController.AddDatasetToChart(displayNameMap[nodeId]);
+                    }
+
+                    // try to add to our list of received values
+                    try
+                    {
+                        SignalRModel newItem = new SignalRModel {
+                            NodeID = displayNameMap[nodeId],
+                            TimeStamp = message.Payload[nodeId].SourceTimestamp,
+                            Value = float.Parse(message.Payload[nodeId].Value.ToString())
+                        };
+                        receivedDataItems.Add(newItem);
+                    }
+                    catch (Exception)
+                    {
+                        // ignore this item
+                    }
+
+                    // add item to our table entries
+                    tableEntries.Add(new Tuple<string, string, string>(
+                        displayNameMap[nodeId],
+                        message.Payload[nodeId].Value.ToString(),
+                        message.Payload[nodeId].SourceTimestamp.ToString()
+                    ));
+                }
+            }
+
+            // update our table in the dashboard
+            DashboardController.CreateTableForTelemetry(tableEntries);
+
+            // update our line chart in the dashboard
+            while (receivedDataItems.Count > 0)
+            {
+                DateTime currentTimestamp = receivedDataItems[0].TimeStamp;
+
+                // add first value from the start of our received data items array
+                _currentValues.Insert(NodeIDs.IndexOf(receivedDataItems[0].NodeID), receivedDataItems[0].Value);
+                _currentValues.RemoveAt(NodeIDs.IndexOf(receivedDataItems[0].NodeID) + 1);
+                receivedDataItems.RemoveAt(0);
+
+                // add the values we received with the same timestamp
+                for (int i = 0; i < receivedDataItems.Count; i++)
+                {
+                    if (receivedDataItems[i].TimeStamp == currentTimestamp)
+                    {
+                        _currentValues.Insert(NodeIDs.IndexOf(receivedDataItems[i].NodeID), receivedDataItems[i].Value);
+                        _currentValues.RemoveAt(NodeIDs.IndexOf(receivedDataItems[0].NodeID) + 1);
+                        receivedDataItems.RemoveAt(i);
+                        i--;
+                    }
+                }
+
+                DashboardController.AddDataToChart(currentTimestamp.ToString(), _currentValues.ToArray());
+            }
+        }
+
+        private void Checkpoint(PartitionContext context, Stopwatch checkpointStopwatch)
+        {
+            context.CheckpointAsync();
+            checkpointStopwatch.Restart();
+            Trace.TraceInformation($"checkpoint completed at {DateTime.UtcNow}");
+        }
+
+        /// <summary>
+        /// Process all events from OPC UA servers and update the last value of each node in the topology.
+        /// </summary>
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> ingestedMessages)
+        {
+            // process each message
+            foreach (var eventData in ingestedMessages)
+            {
+                string message = null;
+                try
+                {
+                    // checkpoint, so that the processor does not need to start from the beginning if it restarts
+                    if (_checkpointStopwatch.Elapsed > TimeSpan.FromMinutes(_checkpointPeriodInMinutes))
+                    {
+                        await Task.Run(() => Checkpoint(context, _checkpointStopwatch)).ConfigureAwait(false);
+                    }
+
+                    message = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                    if (message != null)
+                    {
+                        // support batched messages as well as simple message ingests
+                        if (message.StartsWith("["))
+                        {
+                            List<OCPPChargePoint> publisherMessages = JsonConvert.DeserializeObject<List<OCPPChargePoint>>(message);
+                            foreach (OCPPChargePoint publisherMessage in publisherMessages)
+                            {
+                                if (publisherMessage != null)
+                                {
+                                    try
+                                    {
+                                        ProcessPublisherMessage(publisherMessage);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Trace.TraceError($"Exception '{e.Message}' while processing message {publisherMessage}");
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            OCPPChargePoint publisherMessage = JsonConvert.DeserializeObject<OCPPChargePoint>(message);
+                            if (publisherMessage != null)
+                            {
+                                ProcessPublisherMessage(publisherMessage);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Trace.TraceError($"Exception '{e.Message}' processing message '{message}'");
+                }
+            }
+        }
+    }
+}
+
+
