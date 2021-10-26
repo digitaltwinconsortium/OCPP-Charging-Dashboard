4d4218ffc33098a5e91f8fe49e2090301cde9249
barnstee <erichb@microsoft.com>
2021-10-26 13:48:11 +0200

    Push more SignalR messages initially.

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -32,7 +32,7 @@ namespace OpcUaWebDashboard.Controllers
 
         public ActionResult Index()
         {
-            _timer.Change(5000, 5000);
+            _timer.Change(500, 500);
 
             return View("Index");
         }
